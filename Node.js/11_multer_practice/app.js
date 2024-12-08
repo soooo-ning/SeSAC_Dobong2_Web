1480b9a0ee5d61f64752833a806bb76093235ad3
soooo-ning <ssoning13@gmail.com>
2024-12-08 22:47:45 +0900

    update

--- Node.js/11_multer_practice/app.js
+++ Node.js/11_multer_practice/app.js
@@ -12,6 +12,11 @@ app.use(express.json());
 app.use('/static', express.static(__dirname + '/public'));
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
+// 회원가입 일반 폼 전송에 파일 업로드 연결하기 (form submit)
+// 이 때, 업로드 할 파일은 프로필 사진!
+// 업로드 된 파일은 "uploads/유저아이디.확장자"로 저장하기
+// 업로드 후 결과 페이지에서 업로드 된 이미지 보여주기
+
 const uploadDetail = multer({
   storage: multer.diskStorage({
     destination: function (req, file, done) {
@@ -19,7 +24,7 @@ const uploadDetail = multer({
     },
     filename: function (req, file, done) {
       const extension = path.extname(file.originalname);
-      const filename = `${req.body.id || 'default'}${extension}`;
+      const filename = `${req.body.id}${extension}`;
       done(null, filename);
     },
   }),
@@ -32,10 +37,10 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
-  const { id, password, name, age } = req.body;
-  const profile = req.file.filename;
   console.log(req.body);
   console.log(req.file);
+  const { id, password, name, age } = req.body;
+  const profile = req.file.filename;
 
   res.render('result', {
     id,
