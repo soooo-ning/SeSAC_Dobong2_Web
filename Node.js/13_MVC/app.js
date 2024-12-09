dfba849cad996541589784362a0791dba2066057
soooo-ning <ssoning13@gmail.com>
2024-12-09 13:05:38 +0900

    update

--- Node.js/13_MVC/app.js
+++ Node.js/13_MVC/app.js
@@ -7,6 +7,60 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// const comments = [
+//   {
+//     id: 1,
+//     userid: "apple",
+//     date: "2024-10-23",
+//     comment: "안녕하세요~~~~~~~~~~~~",
+//   },
+//   {
+//     id: 2,
+//     userid: "banana",
+//     date: "2024-11-23",
+//     comment: "반갑습니다!!!!!~~~~~",
+//   },
+//   {
+//     id: 3,
+//     userid: "cocoa",
+//     date: "2024-09-23",
+//     comment: "아무거나 쓰겠습니다...",
+//   },
+//   {
+//     id: 4,
+//     userid: "donut",
+//     date: "2024-10-31",
+//     comment: "곧 점심시간 이네요..",
+//   },
+// ];
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// app.get("/comments", (req, res) => {
+//   console.log(comments);
+//   res.render("comments", { commentInfos: comments });
+// });
+
+// app.get("/comment/:id", (req, res) => {
+//   console.log(req.params);
+//   //   console.log(req.query);
+//   const commentId = req.params.id; //
+//   console.log("commentId:", commentId); // 1,2,3,4
+
+//   console.log(comments[commentId - 1]); // 댓글 실제 정보
+//   if (commentId < 1 || commentId > comments.length) {
+//     res.render("404");
+//   }
+
+//   if (isNaN(commentId)) {
+//     res.render("404");
+//   }
+
+//   res.render("comment", { commentInfo: comments[commentId - 1] });
+// });
+
 // 라우터 분리
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
