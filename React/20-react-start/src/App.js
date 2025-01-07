e1ae8f953c11a0fa2b879a21dc8e3d27c7553fc7
soooo-ning <ssoning13@gmail.com>
2025-01-07 09:40:40 +0900

    학습 파일 추가

---
+++ React/20-react-start/src/App.js
@@ -0,0 +1,21 @@
+import ClassComp from './ClassComp';
+import UseJSX from './UseJSX';
+import Exam01 from './practice/Exam01';
+
+function App() {
+  return (
+    <div>
+      <div></div>
+      <UseJSX />
+      <ClassComp />
+      <Exam01 />
+    </div>
+  );
+}
+
+export default App;
+
+// 삼항 연산자를 사용하여 3 + 5 == 8 이란 수식이 맞으면 "정답입니다!" 를, 틀리면 "오답입니다!" 를 출력하도록 해주세요.
+
+// a라는 변수와 b라는 변수를 만들고 각각에 숫자(정수)를 넣어주세요.
+// &&연산자를 이용하여 a가 b보다 크다면 "a가 b보다 큽니다"를 출력하도록 해주세요.
---
+++ React/20-react-start/src/practice/Exam01.js
@@ -0,0 +1,25 @@
+export default function Exam01() {
+  let name = '두두';
+  let animal = '말티즈';
+
+  const underlineStyle = {
+    textDecoration: 'underline',
+  };
+
+  let a = 3;
+  let b = 2;
+
+  return (
+    <div>
+      <div>
+        제 반려동물의 이름은 <span style={underlineStyle}>{name}</span>입니다
+      </div>
+      <div>
+        <span style={underlineStyle}>{name}</span>는{' '}
+        <span style={underlineStyle}>{animal}</span>입니다.
+      </div>
+      <h2>{3 + 5 === 8 ? '정답입니다.' : '오답입니다.'}</h2>
+      <h2>{a > b && 'a가 b보다 큽니다.'}</h2>
+    </div>
+  );
+}
